6d3ae90af6857d65d11a120d0fb60c1aa4e90e49
gurwinder858 <69290644+gurwinder858@users.noreply.github.com>
2020-08-06 11:01:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,29 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
-};
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBxE9EuPre3m4FBBeM9hXbOmb5Q6FWRee4",
+    authDomain: "hacking-f68cd.firebaseapp.com",
+    databaseURL: "https://hacking-f68cd.firebaseio.com",
+    projectId: "hacking-f68cd",
+    storageBucket: "hacking-f68cd.appspot.com",
+    messagingSenderId: "559407922731",
+    appId: "1:559407922731:web:831d69fae8925fbac7b46e",
+    measurementId: "G-66ZTQ72CLN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
 
      
  
